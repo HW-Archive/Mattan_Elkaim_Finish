6b86bacd33ae15cb45382974e246512ac779126a
Mattan Elkaim <matanelkaim2007@gmail.com>
2023-06-16 14:41:18 +0300

    Add project files.

---
+++ GIF_Maker/prog.c
@@ -0,0 +1,18 @@
+/*********************************
+* Class: MAGSHIMIM C2			 *
+* Week:                			 *
+* Name: Mattan Elkaim            *
+**********************************/
+#pragma warning(disable : 4996) // Ignore unsafe functions
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+	// Your code here...
+	
+	(void)getchar();
+	return 0;
+}
